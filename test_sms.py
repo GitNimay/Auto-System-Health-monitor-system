6db4cdf65712a1916c5f9822af5fcd6b4d6d1d27
GitNimay <167794819+GitNimay@users.noreply.github.com>
2024-11-17 03:28:29 +0530

    Update test_sms.py

--- test_sms.py
+++ test_sms.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Twilio credentials
-account_sid = "AC05f752a99c3de6996e956c2474a9d9ca"
-auth_token = "6e2a543f16c3be18628c33cb600c03bb"
+account_sid = "ADD_YOURS"
+auth_token = "ADD_YOURS"
 client = Client(account_sid, auth_token)
 
 # Sending SMS
 message = client.messages.create(
     body="Hi Nimesh my self you from past few sec!",
-    from_="+18304944676",
-    to="+918767401706"
+    from_="+",
+    to="+"
 )
 
 print(f"Message sent with SID: {message.sid}")
